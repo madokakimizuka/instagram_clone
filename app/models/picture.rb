01f1d118fed8789b47e10e2988319faea4d4bdf0
君塚まどか <kimitaro0415@gmail.com>
2019-03-28 19:11:32 +0900

    写真投稿にバリデーションを定義

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :picture, presence: true
+  validates :content, presence: true
 end
