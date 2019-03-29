f076e1011ed368a1112449ae2e9f0ec9e1ecb827
君塚まどか <kimitaro0415@gmail.com>
2019-03-29 18:24:24 +0900

    デバックのためのgemをインストール

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
+  process resize_to_limit: [200, 200]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -23,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  #
+
   # def scale(width, height)
   #   # do something
   # end
