fa3e144964a1d22851091c3487d5412bd54da30e
君塚まどか <kimitaro0415@gmail.com>
2019-04-02 18:23:28 +0900

    投稿時確認メール送信機能の修正

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: "kimitaro0415@gmail.com", subject: "投稿確認のメール"
+    mail to: "@picture.user.email", subject: "投稿確認のメール"
   end
 end
