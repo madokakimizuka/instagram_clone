51c03fcedfc9f7800daa590e944fe4fa458468fb
君塚まどか <kimitaro0415@gmail.com>
2019-04-13 13:47:16 +0900

    ログインしてなければアクセス制限がかかるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :logged_in_user
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to new_session_path
+    end
+  end
 end
